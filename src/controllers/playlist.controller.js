95019f28999e28dd3bb63735ad55fab9d600037b
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-07-13 19:41:43 +0530

    Implement playlist management features: add create, get user playlists, and get playlist by ID functionalities with validation and aggregation

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -5,21 +5,163 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
+//create playlist
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
+    const {name, description} = req.body;
 
-    //TODO: create playlist
+    if(!name || !description){
+        throw new ApiError(400,"Name and Description both are required");
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user?._id
+    })
+
+    if(!playlist){
+        throw new ApiError(500,"Failed to create PLaylist")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,playlist,"Playlist created successfully")
+    )
+
+
 })
 
+//get user playlists(Return all playlists created by a particular user.)
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
-    //TODO: get user playlists
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400,"Invalid userId")
+    }
+
+    const playlists= await Playlist.aggregate([
+        {
+            $match:{
+                owner : new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"videos",
+                foreignField:"_id",
+                as:"videos"
+            }
+        },
+        {
+            $addFields:{
+                totalVideos:{
+                    $size:"$videos"
+                },
+                totalViews:{
+                    $sum:"$videos.views"
+                }
+            }
+        },
+        {
+            $project:{
+                _id:1,
+                name:1,
+                description:1,
+                totalVideos:1,
+                totalViews:1,
+                updatedAt:1
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse (200,playlists,"User playlists fetched successfully"))
 })
 
+//get playlist by id(Return one specific playlist.)
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
-    //TODO: get playlist by id
+
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400,"Invalid PlaylistId")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if(!playlist){
+        throw new ApiError(404,"Playlist not found")
+    }
+
+    const playlistVideos = await PLaylist/aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(playlistId)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"videos",
+                foreignField:"_id",
+                as:"videos"
+            }
+        },
+        {
+            $match:{
+                "videos.isPublished":true
+            }
+        },
+        {
+            $addFields:{
+                totalVideos:{
+                    $size:"$videos"
+                },
+                totalViews:{
+                        $sum:"$videos.views"
+                },
+                owner:{
+                    $first:"$owner"
+                }
+                
+            }
+        },
+        {
+            $project:{
+                name:1,
+                description:1,
+                createdAt:1,
+                updatedAt:1,
+                totalVideos:1,
+                totalViews:1,
+                videos:{
+                    _id:1,
+                    "videosFile.url":1,
+                    "thumbnail.url":1,
+                    title:1,
+                    description:1,
+                    duration:1,
+                    createdAt:1,
+                    views:1
+                },
+                owner:{
+                    username:1,
+                    fullName:1,
+                    "avatar.url":1
+                }
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,playlistVideos[0],"playlist fetched successfully")
+    )
+
 })
+
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
