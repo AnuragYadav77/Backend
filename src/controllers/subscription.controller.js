a242abce998efe0a4a436a9c85f6b37314b03da4
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-07-16 19:43:11 +0530

    Implement toggle subscription functionality with validation and response handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,9 +6,42 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
+// toggle subscription
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
-    // TODO: toggle subscription
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channelId");
+    }
+
+    const isSubscribed = await Subscription.findOne({
+        subscriber:req.user?._id,
+        channel:channelId
+    })
+    
+    //if already subscribed
+    if(isSubscribed){
+        await Subscription.findByIdAndDelete(isSubscribed?._id);
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200,{subscribed:false},"Unsubscribed successfully")
+        )
+    }
+
+    //if NOT subscribed already
+
+    await Subscription.create({
+        subscriber:req.user?._id,
+        channel:channelId
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{subscribed:true},"Subscribed successfully")
+    )
 })
 
 // controller to return subscriber list of a channel
