af77ba519558907b16307bed5acf4c4b4a8bd6e9
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-07-19 10:00:33 +0530

    Implement update tweet functionality with validation and ownership check

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -115,8 +115,49 @@ const getUserTweets = asyncHandler(async (req, res) => {
     )
 })
 
+//update tweet
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+    const {content} = req.body;
+    const {tweetId} = req.params;
+
+    if (!content) {
+        throw new ApiError(400,"Content is required")
+    }
+
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400,"Invalid tweetId");
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet){
+        throw new ApiError(404,"Tweet not found");
+    }
+
+    if(tweet.owner?.toString()!== req.user?._id.toString()){
+        throw new ApiError(400,"Only owner can edit their tweet");
+    }
+    
+    const newTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{
+                content,
+            }
+        },
+        {
+            new:true
+        }
+    )
+    if(!newTweet){
+        throw new ApiError(500,"Failed to edit tweet, please try again!");
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,newTweet,"Tweet updated successfully")
+    )
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
