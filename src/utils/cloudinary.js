f1a680a0adb5fc3412dc02d9bfdf91a6b9195a39
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-07-19 10:35:53 +0530

    Implement delete functionality for Cloudinary with error handling

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -27,7 +27,21 @@ const uploadOnCloudinary= async(localFilePath)=>{
     }
 }
 
-export {uploadOnCloudinary}
+const deleteOnCLoudinary = async (puublic_id,resource_type="image")=>{
+    try {
+        if(!public_id) return null;
+
+        //delete file from cloudinary
+        const result = await cloudinary.uploader.destroy(public_id,{
+            resource_type:`${resource_type}`
+        });
+    } catch (error) {
+        return error;
+        console.log("Delete on cloudinary failed",error);
+    }
+}
+
+export {uploadOnCloudinary,deleteOnCLoudinary}
 
 
 //     // Upload an image
