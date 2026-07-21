284909452f92b3e0251b8114f0e4fd437780588a
AnuragYadav77 <anuragyadav8871@gmail.com>
2026-07-21 11:41:53 +0530

    Implement toggle publish status functionality with ownership check and error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -414,7 +414,44 @@ const deleteVideo = asyncHandler(async (req, res) => {
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
+
     const { videoId } = req.params
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid VideoId");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if(!video){
+         throw new ApiError(404,"Video not found");
+    }
+
+    if (video?.owner.toString()!== req.user?._id.toString()) {
+        throw new ApiError(400,"You can not toggle publish status as you are not the owner");
+    }
+
+    const toggledVideoPublish = await Video.findByIdAndUpdate(
+        videoId,
+        {
+            $set:{
+                isPublished:!video.isPublished
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    if(!toggledVideoPublish){
+        throw new ApiError(500,"Failed to toggle video publish status")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,{isPublished:toggledVideoPublish.isPublished},"Video publish toggled successfully")
+    )
 })
 
 export {
